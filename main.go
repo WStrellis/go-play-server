482d885535276fc44aaecea56c4a898221e48a5d
Westley Strellis <wstrellis@gmail.com>
2021-04-11 19:14:14 -0400

    add default port 80 if no config file exists

--- main.go
+++ main.go
@@ -4,16 +4,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 const (
-	port = ":57888"
+	configFile = "./config.yml"
 )
 
+type serverConfig struct {
+	Port string `yaml:"port"`
+}
+
+func (s *serverConfig) getConfig(f string) error {
+	if _, err := os.Stat(f); os.IsNotExist(err) {
+		fmt.Println("No configuration file found. Using default configuration.")
+		s.Port = ":80"
+		return nil
+	}
+
+	fBytes, err := os.ReadFile("./config.yml")
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(fBytes, s)
+	if err != nil {
+		return err
+	}
+	return nil
+} // end getConfig
+
 func main() {
+
+	config := serverConfig{}
+	err := config.getConfig(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "./index.html")
 	})
-	fmt.Printf("Server listening on port %v\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	fmt.Printf("Server listening on port %v\n", config.Port)
+	log.Fatal(http.ListenAndServe(config.Port, nil))
 }
